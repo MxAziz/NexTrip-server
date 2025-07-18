12e1f0717473c650e4435f8bb22673d630a14896
MxAziz <hereabdulaziz@gmail.com>
2025-07-18 10:52:04 +0600

    added user routes

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import { Application } from "express";
 import express from "express";
 import cors from "cors";
+import { userRoutes } from "./app/modules/user/user.route";
 
 const app: Application = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/v1/user', userRoutes)
 
 // test route
 app.get("/", (req, res) => {
