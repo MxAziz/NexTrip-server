ed66a1bf2727ae90408a148fd032d9763b811dfb
MxAziz <hereabdulaziz@gmail.com>
2025-07-18 17:18:41 +0600

    split user controller & make service layer

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -3,6 +3,6 @@ import { userController } from "./user.controller";
 
 const router = Router();
 
-router.post("/register", userController.createUser);
+router.post("/register", userController.createUser); // akhane kono vabei createUser ke call kora jabe nah.
 
 export const userRoutes = router;
