220256d09b0555f793cc2fff0284c7dabc960c6b
MxAziz <hereabdulaziz@gmail.com>
2025-07-19 19:49:23 +0600

    added get all users controller with catchAsync fn

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,22 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { UserServices } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
 
-const createUser = async(req: Request, res: Response, next: NextFunction) => {
-    try {
-        const user = await UserServices.createUser(req.body);
-        res
-          .status(httpStatus.CREATED)
-          .json({ message: "user created successfully", user });
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-        // eslint-disable-next-line no-console
-        console.log(error);
-        next(error)
-    }
-}
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const user = await UserServices.createUser(req.body);
+    res.status(httpStatus.CREATED).json({ message: "user created successfully", user });
+  }
+);
+
+const getAllUsers = catchAsync(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const users = await UserServices.getAllUsers();
+        res.status(httpStatus.OK).json({
+            success: true,
+            message: "All users retrieved successfully",
+            data: users
+        })
+  }
+)
 
 
 export const userController = {
-    createUser
+    createUser,
+    getAllUsers,
 }
