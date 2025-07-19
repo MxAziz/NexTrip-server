cea27e1787ebf221c28c6db240d92b231d84c187
MxAziz <hereabdulaziz@gmail.com>
2025-07-19 20:13:58 +0600

    added send response utils

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -4,5 +4,6 @@ import { userController } from "./user.controller";
 const router = Router();
 
 router.post("/register", userController.createUser); // akhane kono vabei createUser ke call kora jabe nah.
+router.get("/all-users", userController.getAllUsers);
 
 export const userRoutes = router;
