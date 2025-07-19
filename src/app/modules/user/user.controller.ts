f6390881193efc7b67ce3428cdee4d37504f1bba
MxAziz <hereabdulaziz@gmail.com>
2025-07-19 20:32:33 +0600

    organized server response method

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,23 +1,32 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express";
-import httpStatus from "http-status-codes";
+import { StatusCodes } from "http-status-codes";
 import { UserServices } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 
 const createUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const user = await UserServices.createUser(req.body);
-    res.status(httpStatus.CREATED).json({ message: "user created successfully", user });
+        sendResponse(res, {
+            success: true,
+            statusCode: StatusCodes.OK,
+            message: "All users retrieved successfully",
+            data: user,
+        })
   }
 );
 
 const getAllUsers = catchAsync(
     async (req: Request, res: Response, next: NextFunction) => {
-        const users = await UserServices.getAllUsers();
-        res.status(httpStatus.OK).json({
+        const result = await UserServices.getAllUsers();
+
+        sendResponse(res, {
             success: true,
+            statusCode: StatusCodes.OK,
             message: "All users retrieved successfully",
-            data: users
+            data: result.data,
+            meta: result.meta,
         })
   }
 )
