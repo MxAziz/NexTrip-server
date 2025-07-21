7c816ed0861c6866ec69dc0b4530f19f67a1d317
MxAziz <hereabdulaziz@gmail.com>
2025-07-21 18:05:16 +0600

    added password hashing system

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,6 +1,8 @@
+import { envVars } from "../../config/env";
 import { AppError } from "../../errorHelpers/AppError";
 import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
+import bcryptjs from "bcryptjs";
 import httpStatus from 'http-status-codes';
 
 const createUser = async(payload: Partial<IUser>) => {
@@ -12,11 +14,13 @@ const createUser = async(payload: Partial<IUser>) => {
         throw new AppError("User Already Exist", httpStatus.BAD_REQUEST)
     }
 
+    const hashedPassword = await bcryptjs.hash(password as string, Number(envVars.BCRYPT_SALT_ROUND))
+
     const authProvider: IAuthProvider = { provider: "credentials", providerId: email as string, };
 
     const user = await User.create({
         email,
-        password,
+        password: hashedPassword,
         auths: [authProvider],
         ...rest,
     })
