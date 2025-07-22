1194f118bcb9e493b64fb1767cd886feafadd344
MxAziz <hereabdulaziz@gmail.com>
2025-07-22 12:06:00 +0600

    added check auth middleware

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { userController } from "./user.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "./user.interface";
 
 const router = Router();
 
 router.post("/register", userController.createUser); // akhane kono vabei createUser ke call kora jabe nah.
-router.get("/all-users", userController.getAllUsers);
+router.get("/all-users",checkAuth(Role.ADMIN, Role.SUPER_ADMIN), userController.getAllUsers);
 
 export const userRoutes = router;
