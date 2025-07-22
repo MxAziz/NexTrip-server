4f2e2c62ee9f66da3922cb24921a351b1c1e6b77
MxAziz <hereabdulaziz@gmail.com>
2025-07-22 11:35:23 +0600

    added jwt and bcrypt salt in env config file

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -3,13 +3,16 @@ import  dotenv  from 'dotenv';
 dotenv.config();
 
 interface EnvConfig {
-    PORT: string,
-    DB_URL: string,
-    NODE_ENV: "development" | "production"
+    PORT: string;
+    DB_URL: string;
+    NODE_ENV: "development" | "production";
+    BCRYPT_SALT_ROUND: string;
+    JWT_ACCESS_SECRET: string;
+    JWT_ACCESS_EXPIRES: string;
 }
 
 const loadEnvVariables = (): EnvConfig => {
-    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV"];
+    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV", "BCRYPT_SALT_ROUND", "JWT_ACCESS_SECRET", "JWT_ACCESS_EXPIRES"];
 
     requiredEnvVariables.forEach(key => {
         if (!process.env[key]) {
@@ -21,6 +24,9 @@ const loadEnvVariables = (): EnvConfig => {
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       DB_URL: process.env.DB_URL!,
       NODE_ENV: process.env.NODE_ENV as "development" | "production",
+      BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
+      JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
+      JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
     };
 }
 
