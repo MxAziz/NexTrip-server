1ec7af5b5a63c8b546e49d5b2d550a98d934c728
MxAziz <hereabdulaziz@gmail.com>
2025-07-24 00:54:30 +0600

    use validate request middleware for patch request

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { userController } from "./user.controller";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { Role } from "./user.interface";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { updateUserZodSchema } from "./user.validation";
 
 const router = Router();
 
 router.post("/register", userController.createUser); // akhane kono vabei createUser ke call kora jabe nah.
 router.get("/all-users",checkAuth(Role.ADMIN, Role.SUPER_ADMIN), userController.getAllUsers);
-router.patch("/:id", checkAuth(...Object.values(Role)), userController.updateUser);
+router.patch("/:id", validateRequest(updateUserZodSchema), checkAuth(...Object.values(Role)), userController.updateUser);
 
 export const userRoutes = router;
