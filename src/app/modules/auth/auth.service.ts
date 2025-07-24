0e4f75e49ec2aacb89fe301790d2fdc03d035c81
MxAziz <hereabdulaziz@gmail.com>
2025-07-24 19:05:04 +0600

    explicit user tokens for cleanner code

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,10 +1,9 @@
 import bcryptjs from "bcryptjs";
 import httpStatus from "http-status-codes";
-import { envVars } from "../../config/env";
 import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 import { AppError } from "../../errorHelpers/AppError";
-import { generateToken } from "../../utils/jwt";
+import { createUserToken } from "../../utils/userTokens";
 
 
 const credentialsLogin = async (payload: Partial<IUser>) => {
@@ -21,22 +20,25 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
     if (!isPasswordMatched) {
         throw new AppError(httpStatus.BAD_REQUEST, "Incorrect Password")
     }
-    const jwtPayload = {
-        userId: isUserExist._id,
-        email: isUserExist.email,
-        role: isUserExist.role
-    }
-    const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES)
+    // const jwtPayload = {
+    //     userId: isUserExist._id,
+    //     email: isUserExist.email,
+    //     role: isUserExist.role
+    // }
+    // const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES)
 
-    const refreshToken = generateToken(jwtPayload, envVars.JWT_REFRESH_SECRET, envVars.JWT_REFRESH_EXPIRES)
+    // const refreshToken = generateToken(jwtPayload, envVars.JWT_REFRESH_SECRET, envVars.JWT_REFRESH_EXPIRES)
 
-    // todo : change password delete method.
-    delete isUserExist.password;
+    const userTokens = createUserToken(isUserExist);
+
+    // delete isUserExist.password;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password: pass, ...rest } = isUserExist.toObject();
 
     return {
-        accessToken,
-        refreshToken,
-        user: isUserExist,
+        accessToken : userTokens.accessToken,
+        refreshToken : userTokens.refreshToken,
+        user: rest,
     }
 
 }
