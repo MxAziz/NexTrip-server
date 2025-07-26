814f49b7fc4ea5a5819cd5b6f5fc09486a201045
MxAziz <hereabdulaziz@gmail.com>
2025-07-26 23:11:38 +0600

    add oauth google strategy

--- src/app/config/passport.ts
+++ src/app/config/passport.ts
@@ -1,6 +1,8 @@
 import passport from "passport";
-import { Strategy as googleStrategy } from "passport-google-oauth20";
+import { Strategy as googleStrategy, Profile, VerifyCallback } from "passport-google-oauth20";
 import { envVars } from "./env";
+import { User } from "../modules/user/user.model";
+import { Role } from "../modules/user/user.interface";
 
 
 passport.use(
@@ -10,6 +12,38 @@ passport.use(
             clientSecret: envVars.GOOGLE_CLIENT_SECRET,
             callbackURL: envVars.GOOGLE_CALLBACK_URL,
 
-        },async()=>{}
+        }, async (accessToken: string, refreshToken: string, profile: Profile, done: VerifyCallback) => {
+            try {
+                const email = profile.emails?.[0].value
+
+                if (!email) {
+                    return done(null, false, {message: "email not found"});
+                }
+
+                let user = await User.findOne({ email });
+
+                if (!user) {
+                    user = await User.create({
+                        email,
+                        name: profile.displayName,
+                        picture: profile.photos?.[0].value,
+                        role: Role.USER,
+                        isVerified: true,
+                        auths: [
+                            {
+                                provider: "google",
+                                providerId: profile.id,
+                            }
+                        ]
+                    })
+                }
+
+                return done(null, user,)
+            } catch (error) {
+                // eslint-disable-next-line no-console
+                console.log("google strategy error", error);
+                return done(error);
+            }
+        }
     )
 )
