64d08651d0dbf4f1190262e149d76fe22c2704b0
MxAziz <hereabdulaziz@gmail.com>
2025-07-28 20:09:05 +0600

    added error types interface file

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -3,11 +3,12 @@ import { userController } from "./user.controller";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { Role } from "./user.interface";
 import { validateRequest } from "../../middlewares/validateRequest";
-import { updateUserZodSchema } from "./user.validation";
+import { createUserZodSchema, updateUserZodSchema } from "./user.validation";
 
 const router = Router();
 
-router.post("/register", userController.createUser); // akhane kono vabei createUser ke call kora jabe nah.
+// api/v1/user/...
+router.post("/register", validateRequest(createUserZodSchema) ,userController.createUser); // akhane kono vabei createUser ke call kora jabe nah.
 router.get("/all-users",checkAuth(Role.ADMIN, Role.SUPER_ADMIN), userController.getAllUsers);
 router.patch("/:id", validateRequest(updateUserZodSchema), checkAuth(...Object.values(Role)), userController.updateUser);
 
