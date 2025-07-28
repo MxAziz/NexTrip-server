c53a4885878a96e90376dcb0ae50d91c03fc2632
MxAziz <hereabdulaziz@gmail.com>
2025-07-28 19:17:28 +0600

    update global error handler

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -6,8 +6,37 @@ import { AppError } from "../errorHelpers/AppError";
 
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
 
+    const errorSources = [
+      {
+        // path: "isDeleted",
+        // message: "cast failed",
+      },
+    ];
+
     let statusCode = 500;
     let message = `something went wrong ${err.message}`;
+
+  // duplicate error
+    if (err.code === 11000) {
+      const matchedArray = err.message.match(/"([^"]*)"/);
+      statusCode = 400;
+      message = `${matchedArray[1]} already exists`;
+    }
+    // object id error / cast error
+    else if (err.name === "CastError") {
+      statusCode = 400;
+      message = "invalid mongodb objectId. please provide a valid id";
+    } else if (err.name === "ValidatorError") {
+      statusCode = 400;
+      const errors = Object.values(err.errors);
+
+      errors.forEach((errorObject: any) => errorSources.push(errorObject.path({
+        path: errorObject.path,
+        message: errorObject.message,
+      })));
+      console.log(errors);
+      message = "validation error occurred";
+    }
 
     if (err instanceof AppError) {
         statusCode = err.statusCode;
@@ -20,7 +49,8 @@ export const globalErrorHandler = (err: any, req: Request, res: Response, next: 
   res.status(statusCode).json({
     success: false,
     message,
-    err,
+    errorSources,
+    // err,
     stack: envVars.NODE_ENV === "development" ? err.stack : null,
   });
 }
