0be08cfbb6a22fbac9273d35ec02bce943025127
MxAziz <hereabdulaziz@gmail.com>
2025-07-30 23:56:23 +0600

    update router index file

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from "express"
 import { userRoutes } from "../modules/user/user.route";
 import { AuthRoutes } from "../modules/auth/auth.route";
+import { DivisionRoutes } from "../modules/division/division.route";
+import { TourRoutes } from "../modules/tour/tour.route";
 
 export const router = Router();
 
+// api/v1/...
 const moduleRoutes = [
     {
         path: "/user",
@@ -12,7 +15,15 @@ const moduleRoutes = [
     {
         path: "/auth",
         route: AuthRoutes
-    }
+    },
+    {
+        path: "/division",
+        route: DivisionRoutes
+    },
+    {
+        path: "/tour",
+        route: TourRoutes
+    },
 ]
 
 moduleRoutes.forEach((route) => {
