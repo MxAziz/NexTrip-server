f9a8f6c294ffe0033d6accf2d0b6133c91fbd657
MxAziz <hereabdulaziz@gmail.com>
2025-08-04 23:50:13 +0600

    update tour routes

--- src/app/modules/tour/tour.route.ts
+++ src/app/modules/tour/tour.route.ts
@@ -1,6 +1,10 @@
 
 import express from 'express';
 import { tourController } from './tour.controller';
+import { checkAuth } from '../../middlewares/checkAuth';
+import { Role } from '../user/user.interface';
+import { validateRequest } from '../../middlewares/validateRequest';
+import { createTourZodSchema, updateTourZodSchema } from './tour.validation';
 
 
 const router = express.Router();
@@ -8,6 +12,28 @@ const router = express.Router();
 /* --------------------- TOUR ROUTES ---------------------- */
 // api/v1/tour/...
 router.get("/", tourController.createTour);
+
+router.post(
+    "/create",
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+    // multerUpload.array("files"),
+    validateRequest(createTourZodSchema),
+    tourController.createTour
+);
+
+router.get(
+    "/:slug",
+    tourController.getSingleTour
+);
+router.patch(
+    "/:id",
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+    // multerUpload.array("files"),
+    validateRequest(updateTourZodSchema),
+    // tourController.updateTour
+);
+
+router.delete("/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), tourController.deleteTour);
 
 
 /* ------------------ TOUR TYPE ROUTES -------------------- */
