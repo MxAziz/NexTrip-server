bfc102948437589c91da1a7e7bf9f3f8fc5f6fa0
MxAziz <hereabdulaziz@gmail.com>
2025-08-04 23:58:22 +0600

    update user route

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -10,6 +10,8 @@ const router = Router();
 // api/v1/user/...
 router.post("/register", validateRequest(createUserZodSchema) ,userController.createUser); // akhane kono vabei createUser ke call kora jabe nah.
 router.get("/all-users",checkAuth(Role.ADMIN, Role.SUPER_ADMIN), userController.getAllUsers);
+// router.get("/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), userController.getSingleUser)
+// router.get("/me", checkAuth(...Object.values(Role)), UserControllers.getMe)
 router.patch("/:id", validateRequest(updateUserZodSchema), checkAuth(...Object.values(Role)), userController.updateUser);
 
 export const userRoutes = router;
