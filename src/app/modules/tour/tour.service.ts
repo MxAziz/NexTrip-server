9836c1321030686cd7a85c412dfb6873a49f36b7
MxAziz <hereabdulaziz@gmail.com>
2025-08-23 19:19:54 +0600

    update get all tours function

--- src/app/modules/tour/tour.service.ts
+++ src/app/modules/tour/tour.service.ts
@@ -18,28 +18,7 @@ const createTour = async (payload: ITour) => {
 
 const getAllTours = async (query: Record<string, string>) => {
 
-    // optional
-    const filter = query;
-    const searchTerm = query.searchTerm || "";
-    const sort = query.sort || "createdAt";
-    const page = Number(query.page) || 1;
-    const limit = Number(query.limit) || 10;
-    const skip = (page - 1) * limit;
-
-
-    const fields = query.fields?.split(",").join(" ") || "";
-
-    delete filter["searchTerm"];
-
-    const searchQuery = {
-        $or : tourSearchableFields.map(field => ({
-            [field]: { $regex: searchTerm, $options: "i" }
-        }))
-    }
-
-    const Tours = await Tour.find(searchQuery).find(filter).sort(sort).select(fields).skip(skip).limit(limit);
-
-    const queryBuilder = new QueryBuilder(Tour.find(), query)
+    const queryBuilder = new QueryBuilder(Tour.find(), query);
 
     const tours = queryBuilder
         .search(tourSearchableFields)
@@ -48,19 +27,10 @@ const getAllTours = async (query: Record<string, string>) => {
         .fields()
         .paginate()
 
-    const totalTours = await Tour.countDocuments(searchQuery).find(filter);
-
-    const meta = {
-        page,
-        limit,
-        total: totalTours,
-        totalPages: Math.ceil(totalTours / limit),
-    }
-
     const [data, meta] = await Promise.all([
-        tours.build(),
-        queryBuilder.getMeta()
-    ])
+      tours.build(),
+      queryBuilder.getMeta(),
+    ]);
 
     return {
         data,
