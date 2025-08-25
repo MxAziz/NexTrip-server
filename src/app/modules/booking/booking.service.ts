7a6939fb76ce39a5ec10545d8497ed676ac68564
MxAziz <hereabdulaziz@gmail.com>
2025-08-26 00:16:54 +0600

    added get transaction id util

--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -1,16 +1,23 @@
 import { AppError } from "../../errorHelpers/AppError";
 import { User } from "../user/user.model";
-import { IBooking } from "./booking.interface";
+import { BOOKING_STATUS, IBooking } from "./booking.interface";
 import httpStatus from 'http-status-codes';
+import { Booking } from "./booking.model";
 
 
-const createBooking = async (payload: IBooking, userId: string) => {
+const createBooking = async (payload: Partial<IBooking>, userId: string) => {
     try {
         const user = await User.findById(userId);
 
         if (!user?.phone || !user.address) {
             throw new AppError(httpStatus.BAD_REQUEST, "Please Update Your Profile to Book a Tour.");
         }
+
+        const booking = await Booking.create({
+            user: userId,
+            status: BOOKING_STATUS.PENDING,
+            ...payload
+        })
     } catch (error) {
         // throw error;
         throw new AppError(httpStatus.BAD_REQUEST, error as string);
