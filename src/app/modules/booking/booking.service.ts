821c4a7e40a9ffe3e5e689297a44fba744cc0ce3
MxAziz <hereabdulaziz@gmail.com>
2025-08-25 23:59:54 +0600

    create booking update to try catch

--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -5,10 +5,15 @@ import httpStatus from 'http-status-codes';
 
 
 const createBooking = async (payload: IBooking, userId: string) => {
-    const user = await User.findById(userId);
+    try {
+        const user = await User.findById(userId);
 
-    if (!user?.phone || !user.address) {
-        throw new AppError( httpStatus.BAD_REQUEST, "Please Update Your Profile to Book a Tour.");
+        if (!user?.phone || !user.address) {
+            throw new AppError(httpStatus.BAD_REQUEST, "Please Update Your Profile to Book a Tour.");
+        }
+    } catch (error) {
+        // throw error;
+        throw new AppError(httpStatus.BAD_REQUEST, error as string);
     }
 }
 
