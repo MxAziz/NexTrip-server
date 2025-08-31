2410e2f397f9326d0ab20849629263ea01115352
MxAziz <hereabdulaziz@gmail.com>
2025-08-31 20:01:29 +0600

    add ssl commerz .env config

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -18,11 +18,16 @@ interface EnvConfig {
   GOOGLE_CALLBACK_URL: string;
   EXPRESS_SESSION_SECRET: string;
   FRONTEND_URL: string;
-
+  SSL: {
+    STORE_ID: string;
+    STORE_PASS: string;
+    SSL_PAYMENT_API: string;
+    SSL_VALIDATION_API: string;
+  }
 }
 
 const loadEnvVariables = (): EnvConfig => {
-    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV", "BCRYPT_SALT_ROUND", "JWT_ACCESS_SECRET", "JWT_ACCESS_EXPIRES", "SUPER_ADMIN_EMAIL", "SUPER_ADMIN_PASSWORD", "JWT_REFRESH_SECRET", "JWT_REFRESH_EXPIRES", "GOOGLE_CLIENT_ID", "GOOGLE_CLIENT_SECRET","GOOGLE_CALLBACK_URL","EXPRESS_SESSION_SECRET","FRONTEND_URL"];
+    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV", "BCRYPT_SALT_ROUND", "JWT_ACCESS_SECRET", "JWT_ACCESS_EXPIRES", "SUPER_ADMIN_EMAIL", "SUPER_ADMIN_PASSWORD", "JWT_REFRESH_SECRET", "JWT_REFRESH_EXPIRES", "GOOGLE_CLIENT_ID", "GOOGLE_CLIENT_SECRET","GOOGLE_CALLBACK_URL","EXPRESS_SESSION_SECRET","FRONTEND_URL", "SSL_STORE_ID", "SSL_STORE_PASS", "SSL_PAYMENT_API", "SSL_VALIDATION_API"];
 
     requiredEnvVariables.forEach(key => {
         if (!process.env[key]) {
@@ -46,6 +51,13 @@ const loadEnvVariables = (): EnvConfig => {
       GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL as string,
       EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET as string,
       FRONTEND_URL: process.env.FRONTEND_URL as string,
+      // ssl
+      SSL: {
+        STORE_ID: process.env.SSL_STORE_ID as string,
+        STORE_PASS: process.env.SSL_STORE_PASS as string,
+        SSL_PAYMENT_API: process.env.SSL_PAYMENT_API as string,
+        SSL_VALIDATION_API: process.env.SSL_VALIDATION_API as string,
+      }
     };
 }
 
